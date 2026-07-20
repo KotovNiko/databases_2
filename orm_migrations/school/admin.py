04c2e56c1e534526e425d7c55e47016528edee9b
admin <kotov1806@gmail.com>
2026-07-20 11:45:16 +0700

    добавление файлов

--- orm_migrations/school/admin.py
+++ orm_migrations/school/admin.py
@@ -5,7 +5,14 @@ from .models import Student, Teacher
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'teacher', 'group']
+    # В list_display НЕЛЬЗЯ ставить teachers (ManyToMany). Ставим метод teachers_list.
+    list_display = ('name', 'group', 'teachers_list')
+
+    def teachers_list(self, obj):
+        """Возвращает строку со всеми учителями студента через запятую."""
+        return ", ".join(teacher.name for teacher in obj.teachers.all())
+
+    teachers_list.short_description = "Учителя"
 
 
 @admin.register(Teacher)
